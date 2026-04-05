9f65c98add4da451d655cc039e3304c6594af477
Banksy <445668+84nk5y@users.noreply.github.com>
2026-04-05 12:59:12 +0200

    Diabled tooltip percentage due to secret values

--- TooltipPercentage.lua
+++ TooltipPercentage.lua
@@ -1,31 +1,28 @@
 local function AddForcesPercentage(tooltip, tooltipData)
-    if not MDT then return end
-
-    local guid = tooltipData.guid
-    if not guid then return end
-
-    local unit = UnitTokenFromGUID(guid)
-    if not unit then return end
+    if not MDT then print("no MDT") return end
 
     local scenarioType = select(10, C_Scenario.GetInfo())
     if scenarioType ~= LE_SCENARIO_TYPE_CHALLENGE_MODE then return end
 
-    local npc_id = tonumber(select(6, strsplit("-", guid)))
-    if not npc_id then return end
+    local guid = tooltipData.guid
+    if not guid then print("no guid") return end
 
-    local count, total = MDT:GetEnemyForces(npc_id)
-    if not count or not total then return end
+    local npcId = select(6, strsplit("-", guid))
+    if not npcId then print("no npcId") return end
+
+    local count, total = MDT:GetEnemyForces(npcId)
+    if not count or not total then print("no GetEnemyForces") return end
 
     local text = format("(+%.2f%%)", count / total * 100)
 
     local prefix = " - Enemy Forces: "
     for i = 2, tooltip:NumLines() do
-        local tooltipText = _G["GameTooltipTextLeft"..i]
-        local lineText = tooltipText and tooltipText:GetText()
+        local leftLine = tooltip:GetTooltipTextObject(i, false)
+        local lineText = leftLine and leftLine:GetText()
+        print("found tooltip line: "..lineText)
 
         if lineText and lineText:find(prefix, 1, true) then
-            tooltipText:SetText(lineText.." "..text)
-            tooltip:Show()
+            leftLine:SetText(lineText.." "..text)
             return
         end
     end
